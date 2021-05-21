89027bc5aa0eaa6e9cfd3c1cb80dc48ff17f49fa
ma-nasa <84346560+ma-nasa@users.noreply.github.com>
2021-05-21 12:25:30 +0530

    Add files via upload

---
+++ conditional statement.py
@@ -0,0 +1,16 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("Enter a number:"))
+Enter a number:8
+>>> b=int(input("Enter a number:"))
+Enter a number:10
+>>> if a<b:
+	print("a is less than b")
+
+	
+a is less than b
+>>> if b<a:
+	print("B is less than a")
+
+	
+>>> 
