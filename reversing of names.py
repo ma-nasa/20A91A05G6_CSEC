c1b3bb2dea931a891da76b7ab33732e204045a30
ma-nasa <84346560+ma-nasa@users.noreply.github.com>
2021-05-25 09:28:56 +0530

    Update reversing of names.py

--- reversing of names.py
+++ reversing of names.py
@@ -1,3 +1,8 @@
 fname = input("Input your First Name : ")
 lname = input("Input your Last Name : ")
 print("Hello  " + lname + " " + fname)
+
+output
+Input your First Name : manasa
+Input your Last Name : smily
+Hello  smily manasa
