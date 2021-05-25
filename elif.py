6523c184daf7ede15e7ea3b141b07c320486c8d1
ma-nasa <84346560+ma-nasa@users.noreply.github.com>
2021-05-25 14:09:06 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a number:"))
+if n%2==0:
+    print("Number is divisble by 2")
+elif n%3==0:
+    print("Number is divisble by 3")
+elif n%5==0:
+    print("Number is divisble by 5")
+else:
+    print("number is not divisble by 2 or 3 or 5")
