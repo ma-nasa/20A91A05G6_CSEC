7a0f63c6ef2b15aa4ac81930251ad2542613ad11
ma-nasa <84346560+ma-nasa@users.noreply.github.com>
2021-05-25 14:10:53 +0530

    Update elif.py

--- elif.py
+++ elif.py
@@ -7,3 +7,12 @@ elif n%5==0:
     print("Number is divisble by 5")
 else:
     print("number is not divisble by 2 or 3 or 5")
+Output
+Enter a number:56
+Number is divisble by 2
+Enter a number:21
+Number is divisble by 3
+Enter a number:25
+Number is divisble by 5
+Enter a number:7
+number is not divisble by 2 or 3 or 5
