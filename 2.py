cabf0d13d9176404e8fd2d4502fc34fe573e405d
ma-nasa <84346560+ma-nasa@users.noreply.github.com>
2021-05-25 08:51:59 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,3 +3,10 @@ print("Python version")
 print(sys.version)
 print("version info.")
 print(sys.version_info)
+
+output
+
+Python version
+3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)]
+version info.
+sys.version_info(major=3, minor=9, micro=5, releaselevel='final', serial=0)
