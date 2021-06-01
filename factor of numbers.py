0b3a99d8efaaf4dbb72214d423986c049d93bbb6
ma-nasa <84346560+ma-nasa@users.noreply.github.com>
2021-06-01 09:08:40 +0530

    Update factor of numbers.py

--- factor of numbers.py
+++ factor of numbers.py
@@ -4,3 +4,11 @@ print("The factors of {} are,".format(number))
 for i in range(1,number+1):
     if number % i == 0:
         print(i)
+        
+output:        
+Enter a number 77
+The factors of 77 are,
+1
+7
+11
+77
